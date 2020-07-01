47e6bae906ded7d03ee27cb3cf4a5b7a0c1c14f2
Harold Pedraza <32584758+harold222@users.noreply.github.com>
2020-06-30 23:30:52 -0500

    changes v.1, add sw

---
+++ js/sw.utils.js
@@ -0,0 +1,12 @@
+
+function actualizarCacheDinamico(dynamicCache, req, resp){
+    if(resp.ok){
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, resp.clone());
+            return resp.clone();
+        });
+    }else{
+        return resp;
+    }
+}
+
