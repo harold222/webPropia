7e856ad12bdcf430c7690481feac0f900f1af053
HaroldP <t1harole@hotmail.com>
2020-07-07 14:45:51 -0500

    starting with api rest

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+require('./server/config');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers');
+const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3000;
+// parse application/x-www-form-urlencoded ,para tipo POST
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/public'));
 
@@ -23,10 +29,60 @@ app.get('/portfolio', (req, res) => {
   res.render('portfolio');
 });
 
+/* =====================================*/
+/* Servicio api rest */
+/* =====================================*/
+
+// get = obntengo todos los proyectos
+app.get('/projects', (req, res) => {
+  res.json('hola');
+});
+
+// post= creo nuevos proyectos
+app.post('/projects', (req, res) => {
+  //el body lo trabaja es body-parser, asi obtengo todo lo que venga en el post urlencoded
+  let body = req.body;
+
+  if(body.name === undefined){
+    //status de peticiones http
+    res.status(400).json({
+      ok: false,
+      message: 'name of project is undefined'
+    });
+  }else if(body.desc === undefined){
+    res.status(400).json({
+      ok: false,
+      message: 'description of project is undefined'
+    });
+  }else{
+    res.status(201).json({
+      project: body,
+      estado: 'update'
+    });
+  }
+
+});
+
+// put = actualizo info de el proyecto con id
+app.put('/projects/:id', (req, res) => {
+  //obtengo el id del que viene por url
+  let id = req.params.id;
+  res.json({
+    id,
+    estado: 'ok'
+  });
+});
+
+// delete = desctivo el proyecto
+app.delete('/projects', (req, res) => {
+  res.json('delete');
+});
+
+
 app.get("*", (req, res) => {
   res.render('404');
 });
 
-app.listen(port, () => {
-  console.log(`Ready on port ${ port }`);
+app.listen(process.env.PORT, () => {
+  console.log(`Ready on port ${ process.env.PORT }`);
 }); 
